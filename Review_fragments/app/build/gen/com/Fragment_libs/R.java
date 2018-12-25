dbc6509383c2656b41cb65fc0145b8e40ae04074
aznable813 <aznable813@gmail.com>
2018-12-25 12:00:14 +0900

    from Note9 12/25 12:00

--- Review_fragments/app/build/gen/com/Fragment_libs/R.java
+++ Review_fragments/app/build/gen/com/Fragment_libs/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.Review_fragments;
+package com.Fragment_libs;
 
 public final class R {
     public static final class attr {
---
+++ Review_fragments/app/src/main/java/com/Review_fragments/MainFragment.java
@@ -0,0 +1,22 @@
+package com.Review_fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.*;
+
+
+// Succeed to Fragment Class
+public class MainFragment extends Fragment
+{
+	
+	//Create View on Fragment
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		// TODO: Implement this method
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+	
+	
+	
+}
