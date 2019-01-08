1a268beb4de79bcabb5699cf22fe2c5702054df3
aznable813 <aznable813@gmail.com>
2019-01-08 14:46:46 +0900

    Temp MainActivity-xml comp from Note9

--- Review_1stDrawer/app/src/main/java/com/Review_1stDrawer/MainActivity.java
+++ Review_1stDrawer/app/src/main/java/com/Review_1stDrawer/MainActivity.java
@@ -2,12 +2,14 @@ package com.Review_1stDrawer;
 
 import android.app.*;
 import android.os.*;
+import android.support.v4.widget.*;
+import android.support.design.widget.NavigationView;
 
 public class MainActivity extends Activity 
 {
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState){
+    
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
